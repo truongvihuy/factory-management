a23a62fac72cd91fc31abdd2bb62064085ea1460
truongvihuy <truongvihuy@gmail.com>
2026-06-13 16:50:57 +0000

    Refactor database module: replace DatabaseModule with PrismaModule and update exports; add Prisma schema and seed files

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,4 +1,3 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -6,13 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
