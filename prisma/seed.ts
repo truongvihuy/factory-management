1b92cccc35d43b56de4c54d7a5682ad55cefe825
truongvihuy <truongvihuy@gmail.com>
2026-06-15 12:53:03 +0000

    Add tsx dependency and update seed script for admin user seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+
+import { PrismaPg } from '@prisma/adapter-pg';
+import bcrypt from 'bcrypt';
+import { Pool } from 'pg';
+
+import { PrismaClient } from '../generated/prisma';
+import * as admin from './data/admin.json';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const _admin = await prisma.user.upsert({
+    where: { email: admin.email },
+    create: {
+      name: admin.name,
+      email: admin.email,
+      password: bcrypt.hashSync(admin.password, 12),
+      status: admin.status,
+      admin: admin.admin,
+    },
+    update: {
+      name: admin.name,
+      email: admin.email,
+      password: bcrypt.hashSync(admin.password, 12),
+      status: admin.status,
+      admin: admin.admin,
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    await pool.end();
+    process.exit(1);
+  });
