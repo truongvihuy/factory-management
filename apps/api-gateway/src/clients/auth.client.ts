c0a441de9dc5ba7d49d58048af4f26617e4e1a28
truongvihuy <truongvihuy@gmail.com>
2026-06-17 15:42:47 +0000

    Implement user permission and admin management methods in AuthClient: add createUser, updateUser, checkAdmin, updateAdmin, getPermissions, checkPermission, getPermission, updatePermission, and deletePermission methods.

--- apps/api-gateway/src/clients/auth.client.ts
+++ apps/api-gateway/src/clients/auth.client.ts
@@ -2,6 +2,7 @@ import { REQUEST_ID, RequestContext, USER_ID } from '@libs/common';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Role, User } from 'generated/prisma';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -53,5 +54,41 @@ export class AuthClient {
 
   async getUser(userId: string, options?: RequestContext) {
     return this._requestServer('get', `${this.url}/user/${userId}`, null, options);
+  }
+
+  async createUser(user: User, options?: RequestContext) {
+    return this._requestServer('post', `${this.url}/user/`, user, options);
+  }
+
+  async updateUser(userId: string, user: User, options?: RequestContext) {
+    return this._requestServer('put', `${this.url}/user/${userId}`, user, options);
+  }
+
+  async checkAdmin(userId: string, options?: RequestContext) {
+    return this._requestServer('get', `${this.url}/user/admin-permission/${userId}`, null, options);
+  }
+
+  async updateAdmin(userId: string, admin: boolean, options?: RequestContext) {
+    return this._requestServer('post', `${this.url}/user/admin-permission/${userId}/${admin}`, null, options);
+  }
+
+  async getPemissions(userId: string, options?: RequestContext) {
+    return this._requestServer('get', `${this.url}/user/permission/${userId}`, null, options);
+  }
+
+  async checkPermission(userId: string, factoryId: string, role: Role, options?: RequestContext) {
+    return this._requestServer('get', `${this.url}/user/permission/${userId}/${factoryId}/${role}`, null, options);
+  }
+
+  async getPermission(userId: string, factoryId: string, options?: RequestContext) {
+    return this._requestServer('get', `${this.url}/user/permission/${userId}/${factoryId}`, null, options);
+  }
+
+  async updatePermission(userId: string, factoryId: string, role: Role, options?: RequestContext) {
+    return this._requestServer('post', `${this.url}/user/permission/${userId}/${factoryId}/${role}`, null, options);
+  }
+
+  async deletePermission(userId: string, factoryId: string, options?: RequestContext) {
+    return this._requestServer('delete', `${this.url}/user/permission/${userId}/${factoryId}`, null, options);
   }
 }
