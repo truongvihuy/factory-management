203469be5ee5c61503bb58979157492a0f3c6cbb
truongvihuy <truongvihuy@gmail.com>
2026-06-18 02:09:46 +0000

    Refactor FactoryController in factory-service: enhance factory management methods and update endpoint structure for improved clarity and functionality.

--- apps/api-gateway/src/modules/factory/factory.controller.ts
+++ apps/api-gateway/src/modules/factory/factory.controller.ts
@@ -1,11 +1,10 @@
-import { Admin, Roles } from '@libs/common';
+import { Admin } from '@libs/common';
 import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
-import { Permission, Role } from 'generated/prisma';
+import { Permission } from 'generated/prisma';
 
 import { AdminGuard } from '../../guards/admin.guard';
 import { FactoryGuard } from '../../guards/factory.guard';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
-import { RoleGuard } from '../../guards/role.guard';
 import { FactoryService } from './factory.service';
 
 @Controller('factory')
@@ -32,32 +31,21 @@ export class FactoryController {
   @Get(':factoryId')
   async getFactory(@Param('factoryId') factoryId: string) {}
 
-  @UseGuards(JwtAuthGuard, AdminGuard)
-  @Admin()
   @Post()
   async addFactory(@Body() body: any) {}
 
-  @UseGuards(JwtAuthGuard, AdminGuard)
-  @Admin()
   @Put(':factoryId')
   async updateFactory(@Param('factoryId') factoryId: string, @Body() body: any) {}
 
-  @UseGuards(JwtAuthGuard, AdminGuard)
-  @Admin()
   @Delete(':factoryId')
   async deleteFactory(@Param('factoryId') factoryId: string) {}
 
-  @UseGuards(JwtAuthGuard, FactoryGuard)
   @Get(':factoryId/workshop')
   async getWorkshopInFactory(@Param('factoryId') factoryId: string) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Post(':factoryId/workshop')
   async addWorkshopInFactory(@Param('factoryId') factoryId: string, @Body() body: any) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Put(':factoryId/workshop/:workshopId')
   async updateWorkshopInFactory(
     @Param('factoryId') factoryId: string,
@@ -65,22 +53,15 @@ export class FactoryController {
     @Body() body: any,
   ) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Delete(':factoryId/workshop/:workshopId')
   async deleteWorkshopInFactory(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string) {}
 
-  @UseGuards(JwtAuthGuard, FactoryGuard)
   @Get(':factoryId/workshop/:workshopId/machine')
   async getMachine(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Post(':factoryId/workshop/:workshopId/machine')
   async addMachine(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string, @Body() body: any) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Put(':factoryId/workshop/:workshopId/machine/:machineId')
   async updateMachine(
     @Param('factoryId') factoryId: string,
@@ -89,8 +70,6 @@ export class FactoryController {
     @Body() body: any,
   ) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Delete(':factoryId/workshop/:workshopId/machine/:machineId')
   async deleteMachine(
     @Param('factoryId') factoryId: string,
@@ -98,7 +77,6 @@ export class FactoryController {
     @Param('machineId') machineId: string,
   ) {}
 
-  @UseGuards(JwtAuthGuard, FactoryGuard)
   @Get(':factoryId/workshop/:workshopId/machine/:machineId/sensor')
   async getSensor(
     @Param('factoryId') factoryId: string,
@@ -106,19 +84,14 @@ export class FactoryController {
     @Param('machineId') machineId: string,
   ) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Post(':factoryId/workshop/:workshopId/machine/:machineId/sensor')
   async addSensor(
     @Param('factoryId') factoryId: string,
     @Param('workshopId') workshopId: string,
     @Param('machineId') machineId: string,
-
     @Body() body: any,
   ) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Put(':factoryId/workshop/:workshopId/machine/:machineId/sensor/:sensorId')
   async updateSensor(
     @Param('factoryId') factoryId: string,
@@ -128,8 +101,6 @@ export class FactoryController {
     @Body() body: any,
   ) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MANAGER)
   @Delete(':factoryId/workshop/:workshopId/machine/:machineId/sensor/:sensorId')
   async deleteSensor(
     @Param('factoryId') factoryId: string,
--- apps/factory-service/src/modules/factory/factory.controller.ts
+++ apps/factory-service/src/modules/factory/factory.controller.ts
@@ -1,22 +1,97 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
-import { Permission } from 'generated/prisma';
 import { FactoryService } from './factory.service';
 
 @Controller('factory')
 export class FactoryController {
   constructor(private readonly factoryService: FactoryService) {}
 
-  @Get('')
-  getFactoryList(@Req() req: any) {
-    const permissions: Permission[] = req.user.permission;
-    const factoryIds = permissions.map((per) => per.factoryId);
-
-    return this.factoryService.getFactoryListByIds(factoryIds);
-  }
-
   @Get('all')
   getFactoryListAll() {
     return this.factoryService.getFactoryListAll();
   }
+
+  @Post('')
+  async getFactoryListByIds(@Body() payload: any) {
+    return this.factoryService.getFactoryListByIds(payload.factoryIds);
+  }
+
+  @Post('create')
+  async addFactory(@Body() body: any) {}
+
+  @Put(':factoryId')
+  async updateFactory(@Param('factoryId') factoryId: string, @Body() body: any) {}
+
+  @Delete(':factoryId')
+  async deleteFactory(@Param('factoryId') factoryId: string) {}
+
+  @Get(':factoryId/workshop')
+  async getWorkshopInFactory(@Param('factoryId') factoryId: string) {}
+
+  @Post(':factoryId/workshop')
+  async addWorkshopInFactory(@Param('factoryId') factoryId: string, @Body() body: any) {}
+
+  @Put(':factoryId/workshop/:workshopId')
+  async updateWorkshopInFactory(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Body() body: any,
+  ) {}
+
+  @Delete(':factoryId/workshop/:workshopId')
+  async deleteWorkshopInFactory(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string) {}
+
+  @Get(':factoryId/workshop/:workshopId/machine')
+  async getMachine(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string) {}
+
+  @Post(':factoryId/workshop/:workshopId/machine')
+  async addMachine(@Param('factoryId') factoryId: string, @Param('workshopId') workshopId: string, @Body() body: any) {}
+
+  @Put(':factoryId/workshop/:workshopId/machine/:machineId')
+  async updateMachine(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+    @Body() body: any,
+  ) {}
+
+  @Delete(':factoryId/workshop/:workshopId/machine/:machineId')
+  async deleteMachine(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+  ) {}
+
+  @Get(':factoryId/workshop/:workshopId/machine/:machineId/sensor')
+  async getSensor(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+  ) {}
+
+  @Post(':factoryId/workshop/:workshopId/machine/:machineId/sensor')
+  async addSensor(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+
+    @Body() body: any,
+  ) {}
+
+  @Put(':factoryId/workshop/:workshopId/machine/:machineId/sensor/:sensorId')
+  async updateSensor(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+    @Param('sensorId') sensorId: string,
+    @Body() body: any,
+  ) {}
+
+  @Delete(':factoryId/workshop/:workshopId/machine/:machineId/sensor/:sensorId')
+  async deleteSensor(
+    @Param('factoryId') factoryId: string,
+    @Param('workshopId') workshopId: string,
+    @Param('machineId') machineId: string,
+    @Param('sensorId') sensorId: string,
+  ) {}
 }
