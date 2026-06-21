acf3de4f9f07d5a479930afe43f8c7104c72ee32
truongvihuy <truongvihuy@gmail.com>
2026-06-21 06:53:54 +0000

    Refactor AppModule to include global ConfigModule and RequestIdServiceMiddleware for enhanced configuration and request handling

--- apps/factory-service/src/app.module.ts
+++ apps/factory-service/src/app.module.ts
@@ -1,6 +1,19 @@
-import { Module } from '@nestjs/common';
+import { RequestIdServiceMiddleware } from '@libs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { FactoryModule } from './modules/factory/factory.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'env/.env.factory-service',
+    }),
+    FactoryModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdServiceMiddleware).forRoutes('*');
+  }
+}
