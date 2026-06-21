c53a4ff9990a25f3c74bdf2de0d98b7afae74610
truongvihuy <truongvihuy@gmail.com>
2026-06-21 07:25:49 +0000

    Update login method to use basicToken and log payload for debugging; add migration for RefreshToken and Session tables

--- apps/auth-service/src/modules/auth/auth.controller.ts
+++ apps/auth-service/src/modules/auth/auth.controller.ts
@@ -13,7 +13,8 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() payload: any) {
-    const { email, password } = this.authHandleService.encoded(payload.token);
+    console.log(payload);
+    const { email, password } = this.authHandleService.encoded(payload.basicToken);
     const { ip, userAgent } = payload;
     return this.authService.login(email, password, ip, userAgent);
   }
