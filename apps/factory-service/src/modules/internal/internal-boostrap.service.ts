ab363fe903cc6dcc39db92cc6552f27ca39ad417
truongvihuy <truongvihuy@gmail.com>
2026-06-23 13:20:21 +0000

    feat(internal): add endpoints for machines, sensors, and devices in bootstrap controller and service

--- apps/factory-service/src/modules/internal/internal-boostrap.service.ts
+++ apps/factory-service/src/modules/internal/internal-boostrap.service.ts
@@ -4,4 +4,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class InternalBootstrapService {
   constructor(private readonly prisma: PrismaService) {}
+
+  getMachines() {
+    return this.prisma.machine.findMany();
+  }
+
+  getSensors() {
+    return this.prisma.sensor.findMany();
+  }
+  getDevices() {
+    return this.prisma.device.findMany();
+  }
 }
