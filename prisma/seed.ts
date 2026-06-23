a4d69f8339258b0c7d51b580c13df552e004fdf0
truongvihuy <truongvihuy@gmail.com>
2026-06-23 08:02:41 +0000

    feat(seed): enhance seeding logic with device and updated factory/workshop structure

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import bcrypt from 'bcrypt';
 import { Pool } from 'pg';
-import { PrismaClient, Role, Status_Machine, Status_Sensor } from '../generated/prisma';
+import { PrismaClient, Role, Status_Device, Status_Machine, Status_Sensor } from '../generated/prisma';
 
 const Metrics = [
   'temperature',
@@ -47,6 +47,7 @@ async function initDb() {
   await prisma.session.deleteMany();
   await prisma.userRole.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.device.deleteMany();
   await prisma.sensor.deleteMany();
   await prisma.machine.deleteMany();
   await prisma.workshop.deleteMany();
@@ -54,7 +55,7 @@ async function initDb() {
 }
 
 async function seedDBUser() {
-  const admin = {
+  const admin_ = {
     name: 'admin',
     email: 'admin@abc.co',
     password: bcrypt.hashSync('123456', 10),
@@ -62,41 +63,41 @@ async function seedDBUser() {
     admin: true,
   };
   await prisma.user.upsert({
-    where: { email: admin.email },
-    create: admin,
-    update: admin,
+    where: { email: admin_.email },
+    create: admin_,
+    update: admin_,
   });
 
   const factories = await prisma.factory.findMany();
 
   for (let [_i, role] of Object.values(Role).entries()) {
     for (let [index, fac] of factories.entries()) {
-      let user = {
+      const user_ = {
         name: `${role.toLowerCase()} ${index}`,
         email: `${role.toLowerCase()}${index}@abc.co`,
-        password: admin.password,
+        password: admin_.password,
         status: true,
         admin: false,
       };
-      let user_ = await prisma.user.upsert({
-        where: { email: user.email },
-        create: user,
-        update: user,
+      let user = await prisma.user.upsert({
+        where: { email: user_.email },
+        create: user_,
+        update: user_,
       });
-      let userRole = {
+      const userRole_ = {
         factoryId: fac.id,
-        userId: user_.id,
+        userId: user.id,
         role: role as Role,
       };
       await prisma.userRole.upsert({
         where: {
           userId_factoryId: {
-            userId: user_.id,
+            userId: user.id,
             factoryId: fac.id,
           },
         },
-        create: userRole,
-        update: userRole,
+        create: userRole_,
+        update: userRole_,
       });
     }
   }
@@ -104,41 +105,77 @@ async function seedDBUser() {
 
 async function seedDBFactory() {
   for (let a = 0; a < 10; a++) {
-    const fac = await prisma.factory.create({
-      data: {
-        name: `Factory ${a + 1}`,
-        address: `123 Abc, HCMC, VN`,
-      },
+    const fac_ = {
+      name: `Factory ${a + 1}`,
+      code: `FAC_${a + 1}`,
+      address: `123 Abc, HCMC, VN`,
+    };
+    const fac = await prisma.factory.upsert({
+      where: { code: fac_.code },
+      create: fac_,
+      update: fac_,
     });
 
     for (let b = 0; b < 2; b++) {
-      const wor = await prisma.workshop.create({
-        data: {
-          name: `Workshop ${a + 1}_${b + 1}`,
-          factoryId: fac.id,
+      const wor_ = {
+        name: `Workshop ${b + 1}`,
+        code: `WS_${b + 1}`,
+        factoryId: fac.id,
+      };
+      const wor = await prisma.workshop.upsert({
+        where: {
+          factoryId_code: {
+            factoryId: wor_.factoryId,
+            code: wor_.code,
+          },
         },
+        create: wor_,
+        update: wor_,
       });
 
       for (let c = 0; c < 50; c++) {
-        const mac = await prisma.machine.create({
-          data: {
-            name: `Machine ${a + 1}_${b + 1}_${c + 1}`,
-            workshopId: wor.id,
-            infoMachine: `Thiss is description Machine ${a + 1}_${b + 1}_${c + 1}`,
-            status: Status_Machine.RUNNING,
-            installDate: new Date(),
-          },
+        const mac_ = {
+          name: `Machine ${c + 1}`,
+          code: `${fac_.code}-${wor_.code}-MAC_${c + 1}`,
+          workshopId: wor.id,
+          infoMachine: `This is description Machine `,
+          status: Status_Machine.RUNNING,
+          installDate: new Date(),
+        };
+        const mac = await prisma.machine.upsert({
+          where: { code: mac_.code },
+          create: mac_,
+          update: mac_,
+        });
+
+        const device_ = {
+          name: `Device ${c + 1}`,
+          code: `${mac_}-DEV_${c + 1}`,
+          machineId: mac.id,
+          secretKey: `DEVICE_${c + 1}`,
+          status: Status_Device.ACTIVE,
+        };
+        const dev = await prisma.device.upsert({
+          where: { code: device_.code },
+          create: device_,
+          update: device_,
         });
 
         for (let d = 0; d < 10; d++) {
-          const sen = await prisma.sensor.create({
-            data: {
-              name: `Sensor ${a + 1}_${b + 1}_${c + 1}_${Metrics[d]}${d + 1}`,
-              machineId: mac.id,
-              metric: Metrics[d],
-              unit: Units[d],
-              status: Status_Sensor.ACTIVE,
+          const sen_ = {
+            name: `Sensor ${Metrics[d]} ${d + 1}`,
+            code: `${mac_}-SEN_${d + 1}`,
+            machineId: mac.id,
+            metric: Metrics[d],
+            unit: Units[d],
+            status: Status_Sensor.ACTIVE,
+          };
+          const sen = await prisma.sensor.upsert({
+            where: {
+              code: `${mac_.code}-SEN_${d + 1}`,
             },
+            create: sen_,
+            update: sen_,
           });
         }
       }
